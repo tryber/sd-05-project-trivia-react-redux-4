7466e8c50936a2fcaec99622fe991afdc22a1e9d
luiz-avelar <luiz.avelar25@gmail.com>
2020-08-18 20:10:32 -0300

    Home page requirements completed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,33 +1,62 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
-function inputForm() {
-  return (
-    <form>
-      <label htmlFor="player-email">E-mail do Gravatar</label>
-      <input type="email" id="player-email" data-testid="input-gravatar-email" />
-      <label htmlFor="player-name">Nome do jogador</label>
-      <input type="text" id="player-name" data-testid="input-player-name" />
-      <Link to="/game">
-        <button type="button" data-testid="btn-play">Jogar</button>
-      </Link>
-    </form>
-  );
-}
+import tokenRequest from '../service/api';
 
 function configButton() {
   return (
     <Link to="/settings">
-      <button type="button" data-testid="btn-settings">Configurações</button>
+      <button type="button" data-testid="btn-settings">
+        Configurações
+      </button>
     </Link>
   );
 }
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      name: '',
+    };
+  }
+
+  inputForm() {
+    const { name, email } = this.state;
+    return (
+      <form>
+        <label htmlFor="player-email">E-mail do Gravatar</label>
+        <input
+          type="email" id="player-email"
+          data-testid="input-gravatar-email"
+          value={email}
+          onChange={(event) => this.setState({ email: event.target.value })}
+        />
+        <label htmlFor="player-name">Nome do jogador</label>
+        <input
+          type="text" id="player-name"
+          data-testid="input-player-name"
+          value={name}
+          onChange={(event) => this.setState({ name: event.target.value })}
+        />
+        <Link to="/game">
+          <button
+            type="button"
+            data-testid="btn-play"
+            onClick={() => tokenRequest()}
+            disabled={(name.length && email.length) === 0}
+          >
+            Jogar
+          </button>
+        </Link>
+      </form>
+    );
+  }
+
   render() {
     return (
       <section>
-        {inputForm()}
+        {this.inputForm()}
         {configButton()}
       </section>
     );
