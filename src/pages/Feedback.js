12fb83a8e96243ef2d1ba215b6e7471718bf0a0b
Preto115 <preto115@gmail.com>
2020-08-23 21:24:13 -0300

    Tela de ranking faltando finalizar a parte 2

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -2,7 +2,32 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import md5 from 'crypto-js/md5';
 
+function playerRankings(avatar, results) {
+  const newPlayer = {
+    name: results.player.name,
+    score: results.player.score,
+    picture: avatar,
+  };
+
+  if (!localStorage.getItem('ranking')) {
+    localStorage.setItem('ranking', JSON.stringify([newPlayer]));
+  } else {
+    const rankings = [...JSON.parse(localStorage.getItem('ranking')), newPlayer];
+    localStorage.setItem('ranking', JSON.stringify(rankings));
+  }
+}
+
+const buttonRanking = (avatar, results) => (
+  <button
+    data-testid="btn-ranking"
+    onClick={() => playerRankings(avatar, results)}
+  >
+    Ver Ranking
+  </button>
+);
+
 class Feedback extends React.Component {
+
   render() {
     const avatar = localStorage.getItem('token');
     const results = JSON.parse(localStorage.getItem('state'));
@@ -27,7 +52,7 @@ class Feedback extends React.Component {
           <button data-testid="btn-play-again">Jogar novamente</button>
         </Link>
         <Link to="/ranking">
-          <button data-testid="btn-ranking">Ver Ranking</button>
+          {buttonRanking(avatar, results)}
         </Link>
       </div>
     );
--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Ranking extends React.Component {
+
   render() {
     return (
-      <h1 data-testid="ranking-title" >Ranking</h1>
+      <div>
+        <h1 data-testid="ranking-title" >Ranking</h1>
+        <Link to="/">
+          <button data-testid="btn-go-home">
+            Voltar para a tela de Início
+          </button>
+        </Link>
+        <div>
+          <ul>
+            <li>ranking</li>
+          </ul>
+        </div>
+      </div>
+
     );
   }
 }
